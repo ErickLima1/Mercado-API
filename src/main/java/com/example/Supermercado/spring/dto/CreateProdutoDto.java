928f06b06800b4a02d888a3cab2835238682a9d1
ErickLima <ericklimacosta29@gmail.com>
2024-05-23 16:04:03 -0300

    Produto Dto Atualizado

--- src/main/java/com/example/Supermercado/spring/dto/CreateProdutoDto.java
+++ src/main/java/com/example/Supermercado/spring/dto/CreateProdutoDto.java
@@ -1,8 +1,11 @@
 package com.example.Supermercado.spring.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record CreateProdutoDto(@NotNull(message = "Nome do Produto obrigatória") String nome,
-                               @NotNull(message = "Valor do Produto obrigatória") String valor){
+public record CreateProdutoDto(@NotBlank(message = "Nome do Produto obrigatória") String nome,
+                               @Positive(message = "Valor do Produto é obrigatório") double valor,
+                               @NotNull(message = "ID do Supermercado é obrigatório") Long supermercadoId){
 
 }
